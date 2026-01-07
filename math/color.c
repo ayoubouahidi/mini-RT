5c098b8584e9181f16a03782cc813136649c86b2
Hamza El Younssi <hamel-yo@c3r8p10.1337.ma>
2026-01-07 06:36:24 +0100

    fix : the color multiplication and the ambint light ratio

--- math/color.c
+++ math/color.c
@@ -48,13 +48,13 @@ t_color	mul_color(t_color c1, t_color c2)
 {
 	t_color	new;
 
-	new.r = c1.r * c2.r;
+	new.r = (int)((c1.r / 255.0) * c2.r);
 	if (new.r > 255)
 		new.r = 255;
-	new.g = c1.g * c2.g;
+	new.g = (int)((c1.g / 255.0) * c2.g);
 	if (new.g > 255)
 		new.g = 255;
-	new.b = c1.b * c2.b;
+	new.b = (int)((c1.b / 255.0) * c2.b);
 	if (new.b > 255)
 		new.b = 255;
 	return (new);
